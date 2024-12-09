0f4df38060a16491508c724b536572f998f6b33b
Imasha Senarath <imashadsenarath@gmail.com>
2024-12-09 23:12:33 +0530

    Election card.

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -14,7 +14,7 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  List elections = ['Presidential Elections', 'Parliamentary Elections'];
+  List elections = ['Presidential Elections', 'Parliamentary Elections', 'Presidential Elections', 'Parliamentary Elections'];
 
   void signOut() {
     FirebaseAuth.instance.signOut();
@@ -33,7 +33,7 @@ class _HomeScreenState extends State<HomeScreen> {
               Container(
                 decoration: const BoxDecoration(
                   color: AppColors.primary, // Background color
-                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                  borderRadius: BorderRadius.all(Radius.circular(AppSizes.borderRadius)),
                 ),
                 child: Padding(
                   padding: const EdgeInsets.all(24),
@@ -79,19 +79,69 @@ class _HomeScreenState extends State<HomeScreen> {
                 "Ongoing Elections",
                 style: TextStyle(color: AppColors.textSecondary, fontSize: 16, fontWeight: FontWeight.bold),
               ),
-              ListView.builder(
-                itemCount: elections.length,
-                shrinkWrap: true,
-                itemBuilder: (context, index) {
-                  return Container(
-                    margin: EdgeInsets.only(bottom: 2),
-                    color: AppColors.primary,
-                    height: 20,
-                  );
-                },
+              const SizedBox(height: 20),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: elections.length,
+                  shrinkWrap: true,
+                  itemBuilder: (context, index) {
+                    return ElectionCard(elections: elections, index: index);
+                  },
+                ),
               )
             ],
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class ElectionCard extends StatelessWidget {
+  const ElectionCard({
+    super.key,
+    required this.elections,
+    required this.index,
+  });
+
+  final List elections;
+  final int index;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(bottom: 10),
+      decoration: BoxDecoration(
+        color: Colors.grey.shade200,
+        borderRadius: BorderRadius.circular(AppSizes.borderRadius),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(24),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              elections[index],
+              style: const TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            Row(
+              children: const [
+                Icon(Icons.people),
+                SizedBox(width: 5),
+                Text("20 Voters"),
+                SizedBox(width: 20),
+                Icon(Icons.verified),
+                SizedBox(width: 5),
+                Text("5 Candidates"),
+              ],
+            )
+          ],
         ),
       ),
     );
